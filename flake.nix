d26fa50e76542ca4f1e8ddbe42e8ef4495217121
Youwes09 <yuvi.shen@outlook.com>
2026-03-30 00:04:54 -0500

    Chore: Bump to 0.6.1

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
 
       perSystem = { system, lib, ... }:
         let
-          version = "0.6.0";
+          version = "0.6.1";
 
           pkgs = import inputs.nixpkgs {
             inherit system;
